1ef24a2c9c2936de0878c50cde790a64c553ae32
cathy <cathy@bubulazi.com>
2023-01-07 17:26:42 +0000

    Copia del proyecto testimonio cambiado  de funiones a clases

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import './App.css'
+import freCodecamp from './imagenes/freecodecamp-logo.png'
+import Boton from './componentes/Boton'
+import Contador from './componentes/Contador'
+import React, { useState } from 'react'
+
+class App extends React.Component {
+  constructor() {
+      super() 
+      this.state = {
+        numClics: 0
+      }
+      this.manejarClic = this.manejarClic.bind(this)
+      this.reiniciarContador = this.reiniciarContador.bind(this)
+    }
+
+    manejarClic() {
+      this.setState(({numClics}) => ({numClics:  numClics + 1}))
+    }
+
+    reiniciarContador() {
+      this.setState({numClics: 0})
+    }
+
+    render() {
+      return (
+        <div className="App">
+          <div className='freecodecamp-logo-contenedor'>
+            <img 
+              className='freecodecamp-logo'qq
+              src={freCodecamp}
+              alt='Logo de freeCodecamp' />
+          </div>
+          <div className='contenedor-principal'>
+        <Contador numClics={this.state.numClics}/>
+        <Boton
+          texto='Clic'
+          esBotonDeClic={true}
+          manejarClic={this.manejarClic} />
+        <Boton
+            texto='Reiniciar'
+            esBotonDeClic={false}
+            manejarClic={this.reiniciarContador} />
+          </div>
+        </div>
+      );
+    }
+  
+
+  }
+
+
+
+  // const [numClics, setNumClics] = useState(0)
+
+  // const manejarClic = () => {
+  //   setNumClics(numClics + 1)
+  // }
+
+  // const reiniciarContador = () => {
+  //   setNumClics(0)    
+  // }
+
+
+
+export default App;
